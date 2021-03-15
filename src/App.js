72651dd0f55a2a1863a027da9e195c0d77d3243d
Blastoise <ashutoshkumarranchi1@gmail.com>
2021-03-15 19:29:27 +0530

    App.js and App.css added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Visualizer from "./components/visualizer";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class App extends Component {
+  state = {};
+  render() {
+    return <Visualizer></Visualizer>;
+  }
+}
+
+export default App;
