9faba7f0281a96b349e07cae554440283615c87a
Blastoise <ashutoshkumarranchi1@gmail.com>
2021-03-21 20:21:13 +0530

    Removed the import statement of bootstrap from src/App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
-import Visualizer from "./components/visualizer";
+import Visualizer from "./components/Visualizer";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   state = {};
   render() {
-    return <Visualizer></Visualizer>;
+    return <Visualizer />;
   }
 }
 
