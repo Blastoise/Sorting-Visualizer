5921162b8092575cb1461e7b9d14c8740c734f41
Blastoise <ashutoshkumarranchi1@gmail.com>
2021-03-21 21:02:23 +0530

    selectionSort.js containing the logic for Selection Sort visualization added

---
+++ src/Algorithms/selectionSort.js
@@ -0,0 +1,30 @@
+function selectionSort(array, speed, createColor, createAnimation) {
+  let delay = speed;
+  let arraySize = array.length;
+
+  for (let i = 0; i < arraySize - 1; i++) {
+    let min = i;
+    for (let j = i + 1; j < arraySize; j++) {
+      createColor([min, j], delay, "COMPARE");
+      delay += speed;
+
+      if (array[min] > array[j]) {
+        min = j;
+      }
+
+      createColor([], delay, "NONE");
+      delay += speed;
+    }
+
+    if (min !== i) {
+      createColor([min, i], delay, "SWAP");
+      createAnimation(array, min, i, delay);
+      delay += speed;
+    }
+
+    createColor([], delay, "NONE");
+    delay += speed;
+  }
+}
+
+export default selectionSort;
