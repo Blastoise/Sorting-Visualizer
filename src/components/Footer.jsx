4d76827406f3ca91ab9bb28114eb581957aefb6d
Blastoise <ashutoshkumarranchi1@gmail.com>
2021-04-24 22:04:41 +0530

    Changes: Added Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <h4>
+        Designed and built with <span>♥</span> by{" "}
+        <a href="https://github.com/Blastoise">Ashutosh Kumar</a>
+      </h4>
+    </div>
+  );
+};
+
+export default Footer;
