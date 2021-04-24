84421bc851eb5979d00d4720e0eb055bb76eb0dc
Blastoise <ashutoshkumarranchi1@gmail.com>
2021-04-24 18:20:03 +0530

    Changes: 1. Removed default export of selectionSort function. 2. Added selectionSortInfo object.

--- src/Algorithms/selectionSort.js
+++ src/Algorithms/selectionSort.js
@@ -1,4 +1,4 @@
-function selectionSort(array, speed, createColor, createAnimation) {
+export function selectionSort(array, speed, createColor, createAnimation) {
   let delay = speed;
   let arraySize = array.length;
 
@@ -28,4 +28,26 @@ function selectionSort(array, speed, createColor, createAnimation) {
   return delay;
 }
 
-export default selectionSort;
+export const selectionSortInfo = {
+  name: "Selection Sort",
+  description:
+    "Selection Sort is an in-place comparison sorting algorithm that divides the input list into two parts: the sublist of items already sorted, which is built up from left to right at the front (left) of the list, and the sublist of items remaining to be sorted that occupy the rest of the list. Initially, the sorted sublist is empty and the unsorted sublist is the entire input list. The algorithm proceeds by finding the smallest element in the unsorted sublist, exchanging (swapping) it with the leftmost unsorted element (putting it in sorted order), and moving the sublist boundaries one element to the right.",
+  performance: {
+    worstTime: (
+      <span>
+        O(n<sup>2</sup>)
+      </span>
+    ),
+    averageTime: (
+      <span>
+        O(n<sup>2</sup>)
+      </span>
+    ),
+    bestTime: (
+      <span>
+        O(n<sup>2</sup>)
+      </span>
+    ),
+    worstSpace: <span>O(1)</span>,
+  },
+};
