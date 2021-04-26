33ffc22f08913deaee5d2e41705b394d7932709d
Blastoise <ashutoshkumarranchi1@gmail.com>
2021-04-26 13:13:30 +0530

    Changes: Added class footer-head to Footer Component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div>
-      <h4>
+      <h4 className="footer-head">
         Designed and built with <span>♥</span> by{" "}
         <a href="https://github.com/Blastoise">Ashutosh Kumar</a>
       </h4>
